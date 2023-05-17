1a63aebb2abfe0505095b4231f7066e4314d8e26
Mbogo47 <njokiivy@gmail.com>
2023-05-17 09:33:22 +0300

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,19 @@ function BlogPost() {
       <Header title="Ivy's Blog" className="header" />
       <MainContent
         postTitle="Blog post 1"
-        postContent="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate hic voluptatum dignissimos itaque dolor aspernatur ipsa velit animi aut eveniet, saepe, earum numquam totam sed doloremque illo molestias praesentium sequi repudiandae! Perspiciatis enim soluta repudiandae dolor quis voluptas optio dolorem, excepturi numquam veniam neque voluptatem nobis reiciendis, ratione modi deserunt magnam, similique nam! Eveniet id a odit.Voluptate hic voluptatum dignissimos itaque dolor aspernatur ipsa velit animi aut eveniet, saepe, earum numquam totam sed doloremque illo molestias praesentium sequi repudiandae! Perspiciatis enim soluta repudiandae dolor quis voluptas optio dolorem, excepturi numquam veniam neque voluptatem nobis reiciendis, ratione modi deserunt magnam, similique nam! Eveniet id a odit.  Adipisci, earum?"
+        postContent="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate hic voluptatum dignissimos itaque dolor aspernatur ipsa velit animi aut eveniet, saepe, earum numquam totam sed doloremque illo molestias praesentium sequi repudiandae! Perspiciatis enim soluta repudiandae dolor quis voluptas optio dolorem, excepturi numquam veniam neque voluptatem nobis reiciendis, ratione modi deserunt magnam, similique nam! "
         postHighTitle="Highlight post 1" 
-        postHighContent="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate hic voluptatum dignissimos itaque dolor aspernatur ipsa velit animi aut eveniet, saepe, earum numquam totam sed doloremque illo molestias praesentium sequi repudiandae! Perspiciatis enim soluta repudiandae dolor quis voluptas optio dolorem, excepturi numquam veniam neque voluptatem nobis reiciendis, ratione modi deserunt magnam, similique nam! Eveniet id a odit. Adipisci, earum?" />
+        postHighContent="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate hic voluptatum dignissimos itaque dolor aspernatur ipsa velit animi aut eveniet, saepe, earum numquam totam sed doloremque illo molestias praesentium sequi repudiandae! " />
+      <MainContent
+        postTitle="Blog post 2"
+        postContent="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate hic voluptatum dignissimos itaque dolor aspernatur ipsa velit animi aut eveniet, saepe, earum numquam totam sed doloremque illo molestias praesentium sequi repudiandae! Perspiciatis enim soluta repudiandae dolor quis voluptas optio dolorem, excepturi numquam veniam neque voluptatem nobis reiciendis, ratione modi deserunt magnam, similique nam! "
+        postHighTitle="Highlight post 2"
+        postHighContent="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate hic voluptatum dignissimos itaque dolor aspernatur ipsa velit animi aut eveniet, saepe, earum numquam totam sed doloremque illo molestias praesentium sequi repudiandae! " />
+      <MainContent
+        postTitle="Blog post 3"
+        postContent="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate hic voluptatum dignissimos itaque dolor aspernatur ipsa velit animi aut eveniet, saepe, earum numquam totam sed doloremque illo molestias praesentium sequi repudiandae! Perspiciatis enim soluta repudiandae dolor quis voluptas optio dolorem, excepturi numquam veniam neque voluptatem nobis reiciendis, ratione modi deserunt magnam, similique nam! "
+        postHighTitle="Highlight post 3"
+        postHighContent="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate hic voluptatum dignissimos itaque dolor aspernatur ipsa velit animi aut eveniet, saepe, earum numquam totam sed doloremque illo molestias praesentium sequi repudiandae! " />
       <Footer authorName="Ivy Mbogo" postDate={currentDate} />
     </div>
 
